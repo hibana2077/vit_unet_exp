0385093d5b3a20c6d414c2ae7e9074ffcb215820
hibana2077 <hibana2077@gmail.com>
2024-09-30 18:32:39 +0800

    Refactor train_normal.py to use ResNet50 model instead of ResNet18

--- src/train_normal.py
+++ src/train_normal.py
@@ -45,7 +45,7 @@ for i, (img, label) in enumerate(train_loader):
     break
 
 # define model
-model = resnet18(num_classes=8, in_chans=3)
+model = resnet50(num_classes=8, in_chans=3)
 model = model.to(device)
 
 # define loss function
